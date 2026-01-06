35da946c79a864d5a85e6ad0c0217b04fca31872
Elmas, Berivan <berivan.elmas@haw-hamburg.de>
2026-01-06 19:16:01 +0100

    Aufräumen

--- health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/data/HealthDataCenter.kt
+++ health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/data/HealthDataCenter.kt
@@ -1,11 +1,7 @@
 package com.example.healthapp.data
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.health.connect.client.records.HeartRateVariabilityRmssdRecord
 import com.example.healthapp.presentation.screen.exercisesession.ExerciseSessionViewModel
-import java.time.ZonedDateTime
 
 class HealthDataCenter (private val healthConnectManager: HealthConnectManager){
 
