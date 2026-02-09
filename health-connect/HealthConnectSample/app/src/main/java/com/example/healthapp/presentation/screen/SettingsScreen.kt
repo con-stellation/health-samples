0167e839ed4eee7a1e5ed2fc988a6d5de687cf26
Elmas, Berivan <berivan.elmas@haw-hamburg.de>
2026-02-09 13:50:59 +0100

    Settings Toggle Zustände werden nun gespeichert zwischen Menü-Screen-Wechsel. Aber wenn App angehalten wird, verschwinden die Auswahlen wieder...

--- health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/presentation/screen/SettingsScreen.kt
+++ health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/presentation/screen/SettingsScreen.kt
@@ -26,20 +26,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconToggleButton
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.Help
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -57,9 +52,9 @@ fun SettingsScreen(
     revokeAllPermissions: () -> Unit
 ) {
     val context = LocalContext.current
-    var cloudChecked by remember { mutableStateOf(false) }
-    var tapChecked by remember { mutableStateOf(false) }
-    var employerChecked by remember { mutableStateOf(false) }
+    var cloudChecked by rememberSaveable { mutableStateOf(false) }
+    var tapChecked by rememberSaveable { mutableStateOf(false) }
+    var employerChecked by rememberSaveable { mutableStateOf(false) }
 
 
     Column(
