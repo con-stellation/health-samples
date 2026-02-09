307259a04a7688469ecc9ba0bd6af6572a149264
Elmas, Berivan <berivan.elmas@haw-hamburg.de>
2026-02-09 11:41:39 +0100

    6-3-6-3 gegen Boxbreathing ausgetauscht

--- health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/presentation/screen/userinputs/ExerciseChoiceScreen.kt
+++ health-connect/HealthConnectSample/app/src/main/java/com/example/healthapp/presentation/screen/userinputs/ExerciseChoiceScreen.kt
@@ -48,11 +48,11 @@ fun ExerciseChoiceScreen(
 ) {
 
     var selectedIndex by rememberSaveable { mutableStateOf(-1) }
-    val exerciseChoices = listOf("4-7-8", "4-7-11", "6-3-6-3")
+    val exerciseChoices = listOf("4-7-8", "4-7-11", "4-4-4-4")
     val explanations = listOf(
         "4 Sekunden einatmen (starke Vibration), 7 Sekunden halten (keine Vibration), 8 Sekunden ausatmen (schwache Vibration).",
         "4 Sekunden einatmen (starke Vibration), 7 Sekunden halten (keine Vibration), 11 Sekunden ausatmen (schwache Vibration).",
-        "6 Sekunden einatmen (starke Vibration), 3 Sekunden halten (keine Vibration), 6 Sekunden ausatmen (schwache Vibration), 3 Sekunden halten (keine Vibration)."
+        "4 Sekunden einatmen (starke Vibration), 4 Sekunden halten (keine Vibration), 4 Sekunden ausatmen (schwache Vibration), 4 Sekunden halten (keine Vibration)."
     )
 
     Column(
